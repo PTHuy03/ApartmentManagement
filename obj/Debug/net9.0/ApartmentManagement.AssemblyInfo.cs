d9056dd1b7a3c85d51ba10aac85e7d2334bbd8ec
Phan Trong Huy <pthuy200307@gmail.com>
2025-04-16 23:03:48 +0700

    OPS-11: Update dang nhap

--- obj/Debug/net9.0/ApartmentManagement.AssemblyInfo.cs
+++ obj/Debug/net9.0/ApartmentManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApartmentManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e50c19b7bbfaefbfda5afcdf8c6d3f7a25fbc16")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d25f05f3bb32e22ab760bec01d29a153cbc5aba")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApartmentManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApartmentManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
